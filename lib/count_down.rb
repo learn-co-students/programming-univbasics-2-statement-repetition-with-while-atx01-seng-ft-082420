24014f7551daaf14d9bb7def710d754d7aec6e04
Jake <llira09@gmail.com>
2020-07-15 21:31:15 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,21 @@
 # Write your code here
+# while true do
+#   puts "say this forever..."
+# end
+
+# magic_exit_number = 7
+# count = 0 
+# while count < 10 do
+#   break if count == magic_exit_number
+#   puts "I am the #{count}, I love to count!"
+  
+#   count += 1
+# end
+
+
+count = 10 
+while count >= 1 do
+  puts count
+  count -= 1
+end
+puts "Happy New Year!"
